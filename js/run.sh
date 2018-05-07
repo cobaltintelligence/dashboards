bb2b9336a9bffd7674bcd33674b6dd4b971063dc
Yuan Wang <yuan@yuanagain.com>
2018-05-07 15:35:17 -0500

    boilerplate js

---
+++ js/run.sh
@@ -0,0 +1,11 @@
+#!/bin/run.sh
+docker build -t js_cobalt_dash ./
+
+## If you want to extract stuff from container
+# containerid=$(docker run -d parse -e GCP_VISION_KEY=$GCP_VISION_KEY))
+# echo $containerid
+# docker cp $containerid:/out/ ./
+# docker run -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS -p 80:80 parse
+docker run -e GCP_API_KEY=$GCP_API_KEY -p 80:80 js_cobalt_dash
+
+echo "============ DONE ============"
